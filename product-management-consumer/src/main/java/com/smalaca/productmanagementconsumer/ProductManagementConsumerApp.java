97d6607e15e5588565df2e690bb3a77f555e6ce3
smalaca <ab13.krakow@gmail.com>
2022-06-09 20:45:33 +0200

    Add classes to start user/producer management consumers app

---
+++ product-management-consumer/src/main/java/com/smalaca/productmanagementconsumer/ProductManagementConsumerApp.java
@@ -0,0 +1,11 @@
+package com.smalaca.productmanagementconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagementConsumerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductManagementConsumerApp.class, args);
+    }
+}
---
+++ user-management-consumer/src/main/java/com/smalaca/usermanagementconsumer/UserManagementConsumerApp.java
@@ -0,0 +1,11 @@
+package com.smalaca.usermanagementconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementConsumerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementConsumerApp.class, args);
+    }
+}
